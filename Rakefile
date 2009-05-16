8813b9e095fbfb60afacf9d5a49211759f5f6654
Clayton Ferris <cferris@gmail.com>
2009-05-16 01:57:28 -0700

    basic xmobar setup

--- Rakefile
+++ Rakefile
@@ -1,12 +1,14 @@
 require 'rake'
-
+ 
 desc "install the dotfiles into user's home directory"
 task :install do
   replace_all = false
   Dir['*'].each do |file|
-    next if %w[Rakefile README LICENSE].include? file
+    next if %w[Rakefile README].include? file
     
-    if File.exist?(File.join(ENV['HOME'], ".#{file}"))
+    original = File.join(ENV['HOME'], ".#{file}")
+ 
+    if File.exist?(original) || File.symlink?(original)
       if replace_all
         replace_file(file)
       else
@@ -28,12 +30,12 @@ task :install do
     end
   end
 end
-
+ 
 def replace_file(file)
   system %Q{rm "$HOME/.#{file}"}
   link_file(file)
 end
-
+ 
 def link_file(file)
   puts "linking ~/.#{file}"
   system %Q{ln -s "$PWD/#{file}" "$HOME/.#{file}"}
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,12 +6,20 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run
 import System.IO
 
 main = do
-    xmonad $ defaultConfig {
-        manageHook = manageDocks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts $ layoutHook defaultConfig
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { borderWidth = 1
+        , normalBorderColor = "grey30"
+        , focusedBorderColor = "#aecf96"
+        , workspaces = ["1:web", "2:dev", "3:comm", "4:misc"]
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
         }
